2041b53a92edcb8e2eee1b835e4f39b61e2a2da1
TorresAzael <48043614+TorresAzael@users.noreply.github.com>
2026-07-15 20:40:25 -0600

    Ordenamiento BubbleSort en ensamblador
    Corrección del programa anterior, ahora evalúa correctamente independientemente del orden, además el tamaño del arreglo puede cambiar ajustando 3 líneas de código mas otras 4 para las variables importantes, todas definidas con asterisco doble (**)

--- Practica2/Ejer3.asm
+++ Practica2/Ejer3.asm
@@ -1,21 +1,36 @@
+;Programa que ordena un arreglo con el metodo de BubbleSort, las lineas marcadas con astericos -> (**) ...
+;... son las importantes ya que definen el tamaño y localizaciones en memoria importantes
 	PROCESSOR 16f877
 	INCLUDE <p16f877.inc>
-
-	;Esta zona tiene que tener direcciones mayores de las asignadas para el arreglo a ordenar				;
+	
+	;Esta zona tiene que tener direcciones mayores de las asignadas para el arreglo a ordenar
+						;Zona (**) importante
 	TEMP	equ H'30'	;Variable Temporar para almacenar R1 actual
 	TAM		equ H'31'	;Tamaño del arreglo
-	I		equ H'32'
+	I		equ H'32'	;Indices
 	J		equ H'33'
 		ORG 0
 		GOTO INICIO
 
 		ORG 5
-LOOPDOS:				
-		MOVF TAM,0
-		MOVWF J
+
+LOOPDOS:	
+		MOVLW 0x20		;INDICE INICIAL (**)
+		MOVWF FSR		;Regresamos a FSR a su primer posicion		
 		DECFSZ I,1
+		MOVF I,0		;Igualamos el valor de J a I para no analizar arreglos una vez ordenados
+		MOVWF J			;En bubble sort se pide que j<n-i-1
+		SUBWF I,0
+		BTFSC STATUS,C
 		GOTO LOOPUNO
 		GOTO $
+
+NOCAMBIO:
+		DECF J
+		MOVF J,0
+		SUBLW 0
+		BTFSC STATUS,Z	;Condicion en caso de no haber ningun cambio
+		GOTO LOOPDOS
 
 LOOPUNO:				;Loop Interno
 		GOTO EVALUAR
@@ -26,10 +41,10 @@ EVALUAR:
 		INCF FSR		;En caso de no entrar al "CAMBIO", ya recorremos el arreglo
 		MOVF INDF,0
 		SUBWF TEMP,0	;Se pierde W ¡cuidado!
-		BTFSC STATUS,C	
+		BTFSC STATUS,C	;Si el numero es mayor
 		GOTO CAMBIO
+		GOTO NOCAMBIO
 		
-
 CAMBIO:
 		MOVF INDF, 0	;Tomo el menos y guardo en W
 		DECF FSR		;Regreso
@@ -40,12 +55,13 @@ CAMBIO:
 		DECFSZ J,1
 		GOTO LOOPUNO
 		GOTO LOOPDOS
+
 INICIO:
 		BCF STATUS,RP1	;RP1 valor 0
 		BCF STATUS,RP0	;RP0 valor 0 - Seleccionamos Banco 0
-		MOVLW 0x20		;Asignamos un 20 a W
+		MOVLW 0x20		;Asignamos un 20 a W (**)
 		MOVWF FSR		;Mover el valor de W a FSR
-		MOVLW 0X25		;Aisgnamos el limite de la lectura
+		MOVLW 0X2F		;Aisgnamos el limite de la lectura (**)
 		MOVWF TAM
         MOVF FSR,0
 		SUBWF TAM,1		;Obtenemos el tamaño del arreglo
