b016a2caa0f480cc4866926ab2d61f9298a9fce0
TorresAzael <48043614+TorresAzael@users.noreply.github.com>
2026-07-22 17:26:09 -0600

    Add files via upload

---
+++ Practica4/Ejer1.asm
@@ -0,0 +1,37 @@
+		PROCESSOR 16F877
+		INCLUDE <P16F877.INC>
+
+		Op	equ	H'30'
+
+		ORG 0 ;Vector de reset
+		GOTO INICIO
+
+		ORG 5
+
+LEERA:
+		MOVF TRISA,0
+		MOVWF Op
+		BTFSC Op,0
+		GOTO OPCION1	;Todo es 1s
+		GOTO OPCION2	;Todo es 0s
+OPCION1:
+		MOVLW B'00000000'
+		MOVWF TRISB
+		GOTO LEERA
+OPCION2:
+		MOVLW H'FF'
+		MOVWF TRISB
+		GOTO LEERA
+
+INICIO: CLRF PORTA 		;Limpia PORTA
+		BSF STATUS,RP0 	;Cambia a banco 1
+		BCF STATUS,RP1
+		MOVLW 06H 		;Define puertos A y E como digitales
+		MOVWF ADCON1
+		MOVLW H'1' 		;Configura puerto A bit 0 como entrada
+		MOVWF TRISA
+		MOVLW B'00000000'
+		MOVWF TRISB
+		BCF STATUS,RP0 	;Cambia al banco 0
+		GOTO LEERA
+		END
